10ad6ee793ea7b905f26be7c5f9f88703c2fa5bd
Elshod <Elshod@DESKTOP-EVQNTO7>
2025-07-26 22:25:59 +0300

    Add ElevatorDbContext for database access

---
+++ Lift.API/Data/ElevatorDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Lift.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lift.API.Data
+{
+    public class ElevatorDbContext : DbContext
+    {
+        public ElevatorDbContext(DbContextOptions<ElevatorDbContext> options) : base(options) { }
+
+        public DbSet<ElevatorStatus> ElevatorStatuses { get; set; }
+        public DbSet<ElevatorRequest> ElevatorRequests { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ElevatorStatus>()
+                .Property(e => e.Direction)
+                .HasConversion<string>();
+
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ElevatorRequest>()
+               .Property(r => r.RequestTime)
+               .HasDefaultValueSql("CURRENT_TIMESTAMP"); // so‘rov vaqti default
+        }
+    }
+}
