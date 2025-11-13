3d1711d941f0e5651c428b06638dfb9dadd1e851
Elshod <elshodibadullayev28@gmail.com>
2025-11-13 23:43:42 +0500

    CODE RUB: Add  Cors

--- Lift.API/Program.cs
+++ Lift.API/Program.cs
@@ -3,6 +3,18 @@ using Lift.API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
+
+// CORS sozlamalarini qo'shish
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", builder =>
+    {
+        builder.WithOrigins("http://localhost:4300")
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -10,18 +22,19 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ElevatorService>();
 builder.Services.AddHostedService<ElevatorBackgroundService>();
 
-
 builder.Services.AddDbContext<ElevatorDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+// CORS middleware'ni qo'shish
+app.UseCors("AllowAngular");
 
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<ElevatorDbContext>();
     dbContext.Database.Migrate();
 }
-
 
 app.UseSwagger();
 app.UseSwaggerUI();
